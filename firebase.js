90dd5e18cf4c776fa82f203009f58ceea4f7a0b0
Jessie Sidhu <jsidhu22@gmail.com>
2019-03-11 11:35:25 -0700

    new firebase key added

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
     // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyB5XCbn8_Cy0CnTXzHzd0qe_Q7g3ylIg0k",
+      authDomain: "happytummy-f3257.firebaseapp.com",
+      databaseURL: "https://happytummy-f3257.firebaseio.com",
+      projectId: "happytummy-f3257",
+      storageBucket: "happytummy-f3257.appspot.com",
+      messagingSenderId: "832652500325"
+    };
+
   firebase.initializeApp(config);
 
   var database = firebase.database();
