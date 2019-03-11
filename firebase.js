9f6241f610beee0f7bf241f7fc7a6c2fe83c05e8
Jessie Sidhu <jsidhu22@gmail.com>
2019-03-11 10:29:23 -0700

    time to push to master

--- firebase.js
+++ firebase.js
@@ -1,38 +1,23 @@
 $(document).ready(function(){
 
     // Initialize Firebase
-
-
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
-  var emailsRef = database.ref("/emails");
   var email = "";
 
-
   $("#submit").on("click", function(event){
-
     event.preventDefault();
-
-
     email = $("#email-input").val().trim();
-
-
     database.ref("/emails").push({
        email: email
     });
-
-
-
-
+    clearForm();
   });
 
   var clearForm = function(){
-
-
-
-
+   $("#email-input").val("");
   }
 
     
